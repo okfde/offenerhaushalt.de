8c880bf7f15a679d8488b5a3f54fed5c70c93e2f
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-17 13:13:43 +0100

    make it a python project

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='offenerhaushalt',
+    version='2.0',
+    description="Visualisierte Haushalte der BRD",
+    long_description='',
+    classifiers=[
+        ],
+    keywords='',
+    author='Open Knowledge Foundation Deutschland',
+    author_email='info@offenerhaushalt.de',
+    url='http://okfn.de',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
