0216cc1e0e6b0dae5119f97d183686bb9e7a7ee0
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-18 20:29:32 +0100

    Copy over page on the finances of the European union.

--- offenerhaushalt/web.py
+++ offenerhaushalt/web.py
@@ -13,8 +13,10 @@ def site(slug):
 @app.route('/page/<path:path>.html')
 def page(path):
     page = pages.get_or_404(path)
+    print page.meta.get('frame')
     template = page.meta.get('template', 'page.html')
-    return render_template(template, page=page)
+    return render_template(template, page=page,
+    	framed=page.meta.get('framed', True))
 
 
 @app.route('/sites.json')
