473a49262e9f2a669b398ad5d513717ed8efe11f
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-18 13:41:24 +0100

    Working D3 map on the home page.

--- offenerhaushalt/static/js/home.js
+++ offenerhaushalt/static/js/home.js
@@ -12,11 +12,10 @@ $(function() {
     	.attr("width", width)
     	.attr("height", height);
 
-    d3.json("/static/img/vg250_laender.json", showMap);
+    d3.json("/static/img/deu.topo.json", showMap);
 
     function showMap(error, de) {
-    	console.log(topojson);
-    	var subunits = topojson.feature(de, de.objects.vg250_bld);
+    	var subunits = topojson.feature(de, de.objects.deu);
 
 	    var projection = d3.geo.mercator()
 	        .center([10.5, 51.35])
@@ -26,24 +25,21 @@ $(function() {
 
 	    var path = d3.geo.path()
 	        .projection(projection);
-	        //.pointRadius(4);
 
-	    
-	    svg.append("path")
-	        .datum(subunits)
-	        .attr("d", path)
-	        .style('fill', '#fcfcfc')
-			.style('stroke', 'gray')
-			.style('stroke-width', 1);
-	    /*
 	    svg.selectAll(".subunit")
-	        .data(subunits.geometries)
+	        .data(subunits.features)
 	      .enter().append("path")
-	        .attr("class", function(d) { return "subunit " + d.properties.name; })
+	        .attr("class", function(d) { return "subunit " + d.properties.code; })
 	        .attr("d", path)
-	        .on("click", function(d) {});
-	    */
+	        .on("mouseover", function(d) {
+	        	d3.select(this).transition().duration(200)
+    				.style({'fill':'#333'});
+	        })
+	        .on("mouseout", function(d) {
+	        	d3.select(this).transition().duration(500)
+    				.style({'fill':'#555'});
+	        })
+	        .on("click", function(d) { console.log(d); });
+	    
     }
-
-	console.log(width, height);
 });
