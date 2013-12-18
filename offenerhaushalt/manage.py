faca5aab98cd11d07c875e356eade35a3be6b538
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-18 09:30:32 +0100

    include d3, topojson in JS builds, GA in main page. build always disables DEBUG.

--- offenerhaushalt/manage.py
+++ offenerhaushalt/manage.py
@@ -9,6 +9,8 @@ manager = Manager(app)
 @manager.command
 def freeze():
     """ Freeze the entire site to static HTML. """
+    app.config['DEBUG'] = False
+    app.config['ASSETS_DEBUG'] = False
     freezer.freeze()
 
 if __name__ == '__main__':
