1be0600564f70f89d4efa1146dca774d848147f0
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-19 20:45:04 +0100

    Factor out path parsing and switch path scheme.

--- offenerhaushalt/static/js/budget.js
+++ offenerhaushalt/static/js/budget.js
@@ -19,34 +19,56 @@ $(function(){
     });    
   }
 
-  function update() {
-    if (!window.location.hash.substring(1).length) {
-      window.location.hash = site.default + '/0/'
-    }
+  function parsePath(hash) {
+    var path = {},    
+        location = hash.substring(1).split('/'),
+        levels = location.slice(1, location.length-1);
 
-    var location = window.location.hash.substring(1).split('/', 3),
-        hierarchyName = location[0],
-        level = parseInt(location[1], 10),
-        args = OSDE.parseArgs(location[2]),
-        hierarchy = site.hierarchies[hierarchyName],
-        cuts = $.extend({}, hierarchy.cuts || {}, args);
+    path.hierarchyName = location[0];
+    path.hierarchy = site.hierarchies[path.hierarchyName];
+    path.level = levels.length;
+    path.bottom = path.level >= (path.hierarchy.drilldowns.length - 1);
+    path.drilldown = path.hierarchy.drilldowns[path.level];
+    path.args = OSDE.parseArgs(location[location.length-1]);
 
-    getData(hierarchy.drilldowns[level], cuts).done(function(data) {
-      var dimension = hierarchy.drilldowns[level];
-      $.each(data.drilldown, function(e, drilldown) {
-        var query = $.extend({}, args);
-        query[dimension] = drilldown[dimension].name;
-        drilldown.url = '#' + hierarchyName + '/' + (level+1) + '/' + OSDE.mergeArgs(query);
-      });
-      treemap.render(data, hierarchy.drilldowns[level]);
+    $.each(levels, function(i, val) {
+      path.args[path.hierarchy.drilldowns[i]] = decodeURIComponent(val);
     });
+    return path;
   }
 
-  function positionNode() {
-    this.style("left", function(d) { return d.x + "px"; })
-        .style("top", function(d) { return d.y + "px"; })
-        .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
-        .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+  function makeUrl(path, modifiers) {
+    var args = $.extend({}, path.args, modifiers),
+        url = '#' + path.hierarchyName + '/';
+
+    $.each(path.hierarchy.drilldowns, function(i, drilldown) {
+      if (args[drilldown]) {
+        url += args[drilldown] + '/';
+        delete args[drilldown];
+      }
+    });
+    return url + OSDE.mergeArgs(args);
+  }
+
+  function update() {
+    if (!window.location.hash.substring(1).length) {
+      window.location.hash = site.default + '/'
+    }
+
+    var path = parsePath(window.location.hash);
+    var cuts = $.extend({}, path.hierarchy.cuts || {}, path.args);
+
+    getData(path.drilldown, cuts).done(function(data) {
+      var dimension = path.drilldown;
+      $.each(data.drilldown, function(e, drilldown) {
+        if (!path.bottom) {
+          var modifiers = {};
+          modifiers[dimension] = drilldown[dimension].name;
+          drilldown.url = makeUrl(path, modifiers);
+        }
+      });
+      treemap.render(data, path.drilldown);
+    });
   }
 
   hashtrack.onhashchange(update);
