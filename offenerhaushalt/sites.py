ce975fd528bf62c6e0a578f0205c723fbadf3359
Friedrich Lindenberg <friedrich@pudo.org>
2013-12-20 13:26:17 +0100

    Extend format to include filters spec.

--- offenerhaushalt/sites.py
+++ offenerhaushalt/sites.py
@@ -1,4 +1,5 @@
 import yaml
+import requests
 from slugify import slugify
 
 
@@ -25,15 +26,59 @@ class SiteCollection(object):
 		return {'sites': self.sites}
 
 
+class Filter(_DataObject):
+
+	def __init__(self, site, data):
+		self.site = site
+		self.data = data
+		self.default = unicode(data.get('default'))
+		self._values = None
+
+	@property
+	def values(self):
+		if self._values is None:
+			field = self.data.get('field')
+			dimension, attribute = field, None
+			if '.' in dimension:
+				dimension, attribute = dimension.split('.', 2)
+
+			res = requests.get('https://openspending.org/api/2/aggregate',
+				params={'dataset': self.site.dataset, 'drilldown': field})
+			
+			self._values = []
+			for drilldown in res.json().get('drilldown'):
+				v = drilldown.get(dimension)
+				if isinstance(v, dict):
+					if not attribute:
+						self.data['field'] = field + '.label'
+					v = v.get(attribute or 'label')
+				self._values.append(v)
+			self._values = list(sorted(self._values))
+		return self._values
+
+	@property
+	def class_name(self):
+		_ = self.values
+		return self.field.replace('.', ' ')
+
+	def to_dict(self):
+		values = self.values
+		data = self.data.copy()
+		data['slug'] = self.slug
+		data['values'] = values
+		return data
+
 class Site(_DataObject):
 
 	def __init__(self, data):
 		self.data = data
 		self.slug = slugify(data.get('slug', data.get('name')))
+		self.filters = [Filter(self, d) for d in data.get('filters', [])]
 
 	def to_dict(self):
 		data = self.data.copy()
 		data['slug'] = self.slug
+		data['filters'] = self.filters
 		return data
 
 
