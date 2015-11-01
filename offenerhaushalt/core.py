c3112b8bf80aeb2f93b0a561234a3b256765e789
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-01 21:41:29 +0100

    Delete JS, switch to sass-based Bootstrap

--- offenerhaushalt/core.py
+++ offenerhaushalt/core.py
@@ -5,14 +5,13 @@ from flask.ext.assets import Environment
 from flask_frozen import Freezer
 from flask_flatpages import FlatPages
 
-
 from offenerhaushalt import default_settings
+
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object(default_settings)
-app.config.from_envvar('OFFENERHAUSHALT_SETTINGS', silent=True)
 
 assets = Environment(app)
 freezer = Freezer(app)
