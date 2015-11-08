e02088c1289c7fa634c932ef8fee88804f428ab8
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-08 23:50:49 +0100

    Begin implementing the API client for SpenDB

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,9 +1,24 @@
 
-offenerhaushalt.controller('BudgetController', ['$scope', 'baseData',
-  function($scope, baseData) {
+offenerhaushalt.controller('BudgetController', ['$scope', '$http', '$q', 'baseData',
+  function($scope, $http, $q, baseData) {
+  var budget = baseData.budgets[baseData.currentBudget],
+      apiUrl = baseData.apiUrl + budget.dataset;
 
-  $scope.budget = baseData.budgets[baseData.currentBudget];
-  
-  console.log("HUHUHU", $scope.budget);
+  $http.get(apiUrl + '/model').then(function(res) {
+    budget.model = res.data;
 
+    var dfds = [];
+    for (var i in budget.filters) {
+      var filter = budget.filters[i];
+      dfds.push($http.get(apiUrl + '/members/' + filter.field));
+    }
+
+    $q.all(dfds).then(function(fmems) {
+      for (var i in budget.filters) {
+        budget.filters[i].values = fmems[i].data.data;
+      }
+      $scope.budget = budget;
+      console.log($scope.budget);
+    });
+  });
 }]);
