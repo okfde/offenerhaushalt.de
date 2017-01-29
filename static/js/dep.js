f7f41eb19d1bcf9bf9badf8fc2b896d19aa97cf6
Maximilian Richt <maxi@richt.name>
2017-01-29 21:53:02 +0100

    add js required for map on index page

---
+++ static/js/dep.js
@@ -0,0 +1,8 @@
+---
+layout: null
+---
+
+{% include_relative _vendor/jquery.min.js %}
+{% include_relative _vendor/handlebars.min.js %}
+{% include_relative _vendor/d3.min.js %}
+{% include_relative _vendor/topojson.js %}
