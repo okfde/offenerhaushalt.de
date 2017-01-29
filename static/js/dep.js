750017751a2af6307182979211dae6915c7ac0d2
Maximilian Richt <maxi@richt.name>
2017-01-29 23:18:37 +0100

    add haushalt layout and js

--- static/js/dep.js
+++ static/js/dep.js
@@ -4,5 +4,8 @@ layout: null
 
 {% include_relative _vendor/jquery.min.js %}
 {% include_relative _vendor/handlebars.min.js %}
+{% include_relative _vendor/accounting.min.js %}
 {% include_relative _vendor/d3.min.js %}
 {% include_relative _vendor/topojson.js %}
+{% include_relative _vendor/hashtrack.js %}
+{% include_relative util.js %}
