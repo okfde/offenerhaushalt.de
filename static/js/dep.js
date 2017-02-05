cc977134631cfd4319bc4f81faf0883b4114e2ca
Maximilian Richt <maxi@richt.name>
2017-02-05 12:20:29 +0100

    add missing bootstrap dependencies

--- static/js/dep.js
+++ static/js/dep.js
@@ -3,6 +3,9 @@ layout: null
 ---
 
 {% include_relative _vendor/jquery.min.js %}
+{% include_relative _vendor/transition.js %}
+{% include_relative _vendor/collapse.js %}
+{% include_relative _vendor/dropdown.js %}
 {% include_relative _vendor/handlebars.min.js %}
 {% include_relative _vendor/accounting.min.js %}
 {% include_relative _vendor/d3.min.js %}
